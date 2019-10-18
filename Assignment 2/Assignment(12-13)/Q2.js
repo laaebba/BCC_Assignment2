d2aba8c50dd5bc007f262735cb7e79ac2e86a196
Laaebba Sundas <46197831+laaebba@users.noreply.github.com>
2019-10-18 22:30:42 +0500

    Add files via upload

---
+++ Assignment 2/Assignment(12-13)/Q2.js
@@ -0,0 +1,12 @@
+int1 = prompt("Enter 1st integer");
+int2 = prompt("Enter 2nd integer");
+
+if ((int1*1)-(int2*1)>0){
+    alert("First integer is greater");
+}
+else if ((int1*1)-(int2*1)==0){
+    alert("Both integers are equal");
+}
+else if ((int1*1)-(int2*1)<0){
+ alert("Second integer is greater");
+}
---
+++ Assignment 2/Assignment(12-13)/Q3.js
@@ -0,0 +1,11 @@
+userIn = prompt("Enter an integer");
+
+if((userIn*1)==0){
+    alert("The number is zero");
+}
+else if ((userIn*1)>0){
+    alert("The number is positive");
+}
+else if ((userIn*1)<0){
+    alert("The number is negative");
+}
