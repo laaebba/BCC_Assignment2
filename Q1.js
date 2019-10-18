0992b28a8297872730dd560267013701d15db47d
Laaebba Sundas <46197831+laaebba@users.noreply.github.com>
2019-10-18 22:40:57 +0500

    Delete Q1.js

--- Q1.js
+++
@@ -1,8 +0,0 @@
-var city= prompt("Enter City name: ");
-    if (city == "Karachi" || "karachi"){
-        alert("Welcome to the city of lights");
-       }
-    else alert("Invalid input");
-    //commit
-    
-
